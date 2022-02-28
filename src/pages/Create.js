a15b89c9b4767e724adbd6759f8e9dc6ccfb260a
triartainyoman <triarta.art@gmail.com>
2022-02-28 11:41:00 +0800

    button

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,19 +1,49 @@
 import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
 
 const Create = () => {
   return (
-    <div>
+    <Container>
       {/* <Typography variant="h1" color="primary" align="center">
         Create a New Note
       </Typography> */}
 
-      <Typography color="secondary" noWrap="true" gutterBottom>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque,
-        dignissimos animi porro accusamus, dolor aliquam facilis voluptate neque
-        adipisci ea, dolores accusantium repellendus alias possimus culpa minima
-        dolorum reprehenderit iusto.
+      <Typography
+        color="textSecondary"
+        variant="h6"
+        component="h2"
+        noWrap
+        gutterBottom
+      >
+        Crate a New Note
       </Typography>
-    </div>
+
+      <Button
+        onClick={() => {
+          console.log("You Clicked");
+        }}
+        type="submit"
+        color="secondary"
+        variant="contained"
+        disableElevation
+      >
+        Submit
+      </Button>
+
+      {/* <Button type="submit" disableElevation>
+        Submit
+      </Button>
+      <Button type="submit" variant="outlined" disableElevation>
+        Submit
+      </Button>
+
+      <ButtonGroup color="secondary" variant="contained">
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+      </ButtonGroup> */}
+    </Container>
   );
 };
 
