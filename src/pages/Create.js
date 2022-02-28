a3d082a8029bdac6b17b5788c2d92484982ee026
triartainyoman <triarta.art@gmail.com>
2022-02-28 11:26:11 +0800

    Typography

---
+++ src/pages/Create.js
@@ -0,0 +1,20 @@
+import Typography from "@material-ui/core/Typography";
+
+const Create = () => {
+  return (
+    <div>
+      {/* <Typography variant="h1" color="primary" align="center">
+        Create a New Note
+      </Typography> */}
+
+      <Typography color="secondary" noWrap="true" gutterBottom>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque,
+        dignissimos animi porro accusamus, dolor aliquam facilis voluptate neque
+        adipisci ea, dolores accusantium repellendus alias possimus culpa minima
+        dolorum reprehenderit iusto.
+      </Typography>
+    </div>
+  );
+};
+
+export default Create;
